ebc6744388262edb4c1c21e22e2ef68d935f7c2e
dominique <domin@DESKTOP-OT2687Q>
2022-03-01 07:37:18 -0600

    id param

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 
@@ -54,6 +57,15 @@ function filterByQuery(query, animalsArray) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
   app.listen(PORT, () => {
